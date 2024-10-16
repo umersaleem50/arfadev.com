805f8a07db91ee91ce091f24d602b1342c8b376b
Umar Saleem <umersaleem50@gmail.com>
2024-10-16 21:41:31 +0500

    ugrade image component

--- components/custom-image.tsx
+++ components/custom-image.tsx
@@ -2,54 +2,53 @@ import { urlFor } from "@/sanity/lib/image";
 import Image, { ImageProps } from "next/image";
 import React from "react";
 
-interface ICustomeImageProps extends ImageProps {
+interface ICustomeImageProps extends Omit<ImageProps, "src" | "alt"> {
   imageOBJ: any;
+  alt?: string;
 }
 
 function CustomImage({
   imageOBJ,
-  placeholder,
+  placeholder = "blur", // defaulting to "blur"
   height,
   width,
-  objectFit,
+  objectFit = "fill", // default value for objectFit
   className,
   priority = false,
   loading = "lazy",
+  alt = "Please enter alt", // default alt text
+  ...rest // using rest to catch remaining ImageProps
 }: ICustomeImageProps) {
   const blurDataURL = urlFor(imageOBJ?.asset || imageOBJ)
     .width(20)
     .height(20)
     .blur(20)
     .url();
+
+  const imageUrl = urlFor(imageOBJ?.asset || imageOBJ).url();
+
   const imageOptions: any = {
-    alt: imageOBJ?.alt || "Please enter alt.",
-    style: { objectFit: objectFit || "fill" },
+    src: imageUrl,
+    alt: alt,
+    className: className,
+    blurDataURL: blurDataURL,
+    placeholder: placeholder,
+    loading: priority ? "eager" : loading, // eager loading if priority is true
+    priority: priority,
+    style: { objectFit },
+    quality: 90,
+    sizes: "(max-width: 600px) 100vw, (max-width: 1200px) 50vw, 40vw",
+    ...rest,
   };
+
   if (width && height) {
     imageOptions["width"] = width;
     imageOptions["height"] = height;
   } else {
-    imageOptions["fill"] = true;
+    imageOptions["fill"] = true; // default to fill if width/height not provided
   }
 
-  if (loading === "eager" && priority) {
-    imageOptions["loading"] = "eager";
-    imageOptions["priority"] = true;
-  }
-
-  return (
-    <Image
-      {...imageOptions}
-      className={className}
-      src={urlFor(imageOBJ?.asset || imageOBJ).url()}
-      blurDataURL={blurDataURL}
-      placeholder={placeholder || "blur"}
-      quality={90}
-      sizes="(max-width: 600px) 100vw, (max-width: 1200px) 50vw, 40vw"
-
-      // layout="responsive"
-    />
-  );
+  return <Image {...imageOptions} />;
 }
 
 export default CustomImage;
