d270aac7b53cb6e3fece921f7f833640df689159
Umar Saleem <umersaleem50@gmail.com>
2025-06-27 16:03:53 +0500

    fixed article cover, illustration bug

--- components/hubspot-contact-form.tsx
+++ components/hubspot-contact-form.tsx
@@ -1,15 +1,27 @@
 "use client";
 
-import HubspotForm from "react-hubspot-form";
+import { useCallback } from "react";
+import Script from "next/script";
 
 const HubspotContactForm = () => {
+  const handleLoad = useCallback(() => {
+    if ((window as any).hbspt) {
+      (window as any).hbspt.forms.create({
+        portalId: "48222835",
+        formId: "d0eb35ed-0415-444d-9f05-7db4ad1023f9",
+        target: "#hubspotForm",
+      });
+    }
+  }, []);
+
   return (
     <>
-      <HubspotForm
-        portalId={"48222835"}
-        formId={"d0eb35ed-0415-444d-9f05-7db4ad1023f9"}
-        loading={<div>Loading...</div>}
+      <Script
+        src="https://js.hsforms.net/forms/v2.js"
+        strategy="afterInteractive"
+        onLoad={handleLoad}
       />
+      <div id="hubspotForm" className="w-full" />
     </>
   );
 };
