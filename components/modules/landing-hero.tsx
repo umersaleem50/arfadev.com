6421afbecce8b951a1864e1dd509562f138d06b1
Umar Saleem <umersaleem50@gmail.com>
2025-06-29 00:42:44 +0500

    fixed illustration issue

--- components/modules/landing-hero.tsx
+++ components/modules/landing-hero.tsx
@@ -7,7 +7,7 @@ import { portableComplex } from "../portable-stucture/portable-complex";
 function LandingHero({ module }: any) {
   const { content } = module;
   return (
-    <div className="w-full h-screen flex flex-col items-center lg:py-16 md:py-14 sm:py-10 py-8 relative">
+    <div className="w-full h-screen flex flex-col items-center lg:py-16 md:py-14 sm:py-10 py-8 relative overflow-x-hidden">
       <div
         className={
           "dark:text-foreground prose-headings:text-foreground prose-h1:leading-none prose-strong:text-foreground prose-blockquote:text-foreground prose-sm prose-blockquote:border-l-4 prose-blockquote:border-primary sm:prose-sm md:prose-base prose-headings:font-serif font-sans prose-stone !max-w-[40rem] text-center leading-snug"
