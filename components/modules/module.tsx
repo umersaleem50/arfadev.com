eb19cf52630c9fbed4f292a821f212e22a726a31
Umar Saleem <umersaleem50@gmail.com>
2026-01-26 10:30:35 +0500

    resolved conflicts

--- components/modules/module.tsx
+++ components/modules/module.tsx
@@ -1,6 +1,7 @@
 import dynamic from "next/dynamic";
 import GuaranteeModule from "./guarantee-module";
-import LandingHero from "./landing-hero";
+
+import { LandingHero } from "./landing-hero";
 import Team from "./team";
 
 const PortfolioModule = dynamic(() => import("./portfolio-module"));
