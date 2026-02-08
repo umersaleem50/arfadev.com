606d1db62c072ec29c405a40514671504e6c20fe
Umar Saleem <umersaleem50@gmail.com>
2026-02-08 13:28:37 +0500

    fixed revalidation

--- app/robots.ts
+++ app/robots.ts
@@ -4,9 +4,8 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: "*",
-      allow: "/"
-      
-      // disallow: "*",
+      allow: "/",
+      disallow: "/studio",
     },
     sitemap: `${process.env.NEXT_PUBLIC_BASE_URL}/sitemap.xml`,
   };
