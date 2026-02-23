82dfb383f02509725e7a6530fc26099803a53f0a
Umar Saleem <umersaleem50@gmail.com>
2026-02-23 12:28:39 +0500

    bento grid

--- components/modules/process/index.tsx
+++ components/modules/process/index.tsx
@@ -1,16 +1,9 @@
-"use client";
-
-import { useRef } from "react";
-
-import AnimatedTabs from "@/components/AnimatedTabs";
 import Section from "@/components/section";
 import Bentogrid from "./BentoGrid";
 
 function OurProcess() {
-  const sectionRef = useRef<HTMLDivElement>(null);
   return (
     <Section
-      sectionRef={sectionRef}
       sectionData={{
         title: "How We Bring Ideas to Life",
         subtitle:
@@ -19,15 +12,6 @@ function OurProcess() {
       }}
     >
       {/* <ProcessCards data={processTempData} /> */}
-
-      <AnimatedTabs
-        tabs={[
-          { id: "1", label: "OnBoarding Flows" },
-          { id: "2", label: "Landing Pages" },
-        ]}
-        variant="pill"
-        className="mb-4"
-      />
 
       <Bentogrid />
     </Section>
